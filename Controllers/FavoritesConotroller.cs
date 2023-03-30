13a685da5bb33360984260ff8fa3458b19715aba
Sinaye Tontsi <yonwaba101@gmail.com>
2023-03-30 21:24:01 +0200

    Layout Page Update

---
+++ Controllers/FavoritesConotroller.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NFLTeamsSessions.Models;
+
+namespace NFLTeamsSessions.Controllers
+{
+    public class FavoritesConotroller:Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            var session = new NFLSession(HttpContext.Session);
+            var model = new TeamListViewModel
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv(),
+                Teams = session.GetMyTeams()
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public RedirectToActionResult Delete()
+        {
+            var session = new NFLSession(HttpContext.Session);
+            session.RemoveMyTeams();
+
+            TempData["message"] = "Favorite teamss cleard";
+
+            return RedirectToAction("Index", new
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv()
+            });
+        }
+    }
+}
